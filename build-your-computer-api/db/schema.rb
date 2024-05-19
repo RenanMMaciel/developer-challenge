219e55e4c47d08d5e8528c135dffc2f81e52a038
Renan Maciel <renancemm@gmail.com>
2024-05-19 17:33:08 -0300

    Correção de bugs nos models

--- build-your-computer-api/db/schema.rb
+++ build-your-computer-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_29_152400) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_18_224900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_152400) do
 
   create_table "orders", force: :cascade do |t|
     t.string "client_name"
-    t.json "order"
+    t.json "components"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
